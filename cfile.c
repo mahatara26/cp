326f1d5575edd7ef42c7226f6398a7a47eec8453
mahatara26 <160766576+mahatara26@users.noreply.github.com>
2024-04-20 23:37:06 -0700

    Update cfile.c
    final_improve version

--- cfile.c
+++ cfile.c
@@ -5,18 +5,15 @@
 //function-1
 int bus_details();
 //function-2
-int book_ticket1();
-int book_ticket2();
-int book_ticket3();
+int book_ticket1(int);
+int book_ticket2(int);
+int book_ticket3(int);
 //function-3
 int pass_details();
 int select_destination();
-int payment();
+int payment(int);
+
 //function-4
-int pay1();
-int pay2();
-int pay3();
-//function-5
 int data_access();
 int view_passenger();
 int view_passenger2();
@@ -29,9 +26,9 @@ int main() {
     int choice;
     do {
         printf("\n--$$$ Bus Reservation System $$$--\n");
-        printf("1. bus details\n");
-        printf("2. book ticket\n");
-        printf("3. passenger details access\n");
+        printf("1. Bus details\n");
+        printf("2. Book ticket\n");
+        printf("3. Passenger details access\n");
         printf("4. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
@@ -82,23 +79,23 @@ int select_destination(){
 int choice1;
     do {
         printf("\n--- select your destination---\n");
-        printf("1. pokhara to kathmandu\n");
-        printf("2. pokhara to chitwan\n");
-        printf("3. pokhara to janakpur\n");
+        printf("1. Pokhara to Kathmandu\n");
+        printf("2. Pokhara to Chitwan\n");
+        printf("3. Pokhara to Janakpur\n");
         printf("4. Exit\n");
         printf("Enter your destination: ");
         scanf("%d", &choice1);
 
         switch (choice1) {
             case 1:
-           
-            book_ticket1();
+          // int a=1;
+            book_ticket1(1);
                 break;
             case 2:
-             book_ticket2();
+             book_ticket2(2);
                 break;
             case 3:
-         book_ticket3();
+         book_ticket3(3);
                 break;
             case 4:
                                 printf("Exiting...\n");
@@ -116,9 +113,9 @@ int data_access(){
     int choice;
     do {
         printf("\n--$$$ data of passenger $$$--\n");
-        printf("1. bus-1\n");
-        printf("2. bus-2\n");
-        printf("3. bus-3\n");
+        printf("1. Bus-1 (Kathmandu)\n");
+        printf("2. Bus-2 (Chitwan)\n");
+        printf("3. Bus -3 (Janakpur)\n");
         printf("4. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
@@ -148,7 +145,7 @@ int data_access(){
 
 
 //switch-2 destination case-1 begin
-int book_ticket1(){//bus-1
+int book_ticket1(int s1){//bus-1
     //int price;
     int fail1;
     FILE *p;
@@ -164,32 +161,32 @@ static int i=0;
       
  
 printf("**ENTER YOUR PERSONAL DETAILS**\n");
-printf("enter the name of passenger\n");      //use of formatted output function
+printf("Enter the name of passenger\n");      //use of formatted output function
 scanf("%s",x[i].name);                              //use of formatted input function
 
-   printf("enter the contact_no of passenger\n");
+   printf("Enter the contact_no of passenger\n");
   scanf("%d",&x[i].contact_no);
   printf("\n");
- fail1=payment();
+ fail1=payment(s1);
  if(fail1==1){
-    printf("\t\t\t\t\t\t\t***TICKET HAS NOT BOOKED***\n");
+    printf("\t\t\t\t\t\t\t***TICKET WAS NOT BOOKED***\n");
     goto x1;//goto loop
  }
  else{
-  printf("\t\t\t\t\t\tyour ticket is booked sucecessfully for kathmandu\n");
+  printf("\t\t\t\t\t\tYour ticket is booked sucecessfully for Kathmandu\n");
  setno=setno+1;
 
  printf("\n");
  printf("\t\t\t\t\t\t\t\tPASSENGER NAME:%s\n",x[i].name);
- printf("\t\t\t\t\t\t\t\tcontact_no:%d\n",x[i].contact_no);
+ printf("\t\t\t\t\t\t\t\tContact_no:%d\n",x[i].contact_no);
  printf("\t\t\t\t\t\t\t\tSEATS NO:%d\n",setno);
-   fprintf(p,"%s","name:");
+   fprintf(p,"%s","Name:");
   fprintf(p,"%s",x[i].name);
-   fprintf(p,"%s","contact_no:");
+   fprintf(p,"%s","Contact_no:");
    
   fprintf(p,"%d",x[i].contact_no);
 
-  fprintf(p,"%s","seatno:");
+  fprintf(p,"%s","Seat_no:");
 
   fprintf(p,"%d",setno);
   i++;
@@ -197,13 +194,13 @@ scanf("%s",x[i].name);                              //use of formatted input fun
   //payment process
   // price();
 
-  x1://got variable
+  x1://goto variable
  fclose(p);
    return 0;
 }//switch-2 destination case-1 end
 
 //switch-2 destination case-2 begin
-int book_ticket2(){//Bus- 2
+int book_ticket2(int s2){//Bus- 2
     //int price;
     int fail2;
     FILE *p1;
@@ -217,35 +214,35 @@ struct passenger x1[44];  //use of concept of array
 static int i=0;
  
 printf("**ENTER YOUR PERSONAL DETAILS**\n");
-printf("enter the name of passenger\n");
+printf("Enter the name of passenger\n");
 scanf("%s",x1[i].name);
 
-   printf("enter the contact_no of passenger\n");
+   printf("Enter the contact_no of passenger\n");
   scanf("%d",&x1[i].contact_no);
   printf("\n");
-  fail2=payment();
+  fail2=payment(s2);
   //payment process
   // price();
    if(fail2==1){
-    printf("\t\t\t\t\t\t\t***TICKET HAS NOT BOOKED***\n");
+    printf("\t\t\t\t\t\t\t***TICKET WAS NOT BOOKED***\n");
     goto x2;//goto loop
  }
  else{
 
-  printf("\t\t\t\t\tyour ticket is booked sucecessfully for chitwan\n");
+  printf("\t\t\t\t\tYour ticket is booked sucecessfully for Chitwan\n");
   
  setno1=setno1+1;
 
  printf("\n");
  printf("\t\t\t\t\t\t\tPASSENGER NAME:%s\n",x1[i].name);
- printf("\t\t\t\t\t\t\tCITIZEN NO:%d\n",x1[i].contact_no);
+ printf("\t\t\t\t\t\t\tcontact_no:%d\n",x1[i].contact_no);
  printf("\t\t\t\t\t\t\tSEATS NO:%d\n",setno1);
-  fprintf(p1,"%s","name:");
+  fprintf(p1,"%s","Name:");
   fprintf(p1,"%s",x1[i].name);
-   fprintf(p1,"%s","contact_no:");
+   fprintf(p1,"%s","Contact_no:");
   fprintf(p1,"%d",x1[i].contact_no);
  
-  fprintf(p1,"%s","seatno:");
+  fprintf(p1,"%s","Seat_no:");
 
   fprintf(p1,"%d",setno1);
   i++;
@@ -257,7 +254,7 @@ scanf("%s",x1[i].name);
 }//switch-2 destination case-2 end
 
 //switch-2 destination case-3 begin
-int book_ticket3(){//bus-3
+int book_ticket3(int s3){//bus-3
     //int price;
     int fail3;
     FILE *p2;
@@ -273,33 +270,33 @@ static int i=0;
 
 printf("**ENTER YOUR PERSONAL DETAILS**\n");
 printf("\n");
-printf("enter the name of passenger\n");
+printf("Enter the name of passenger\n");
 scanf("%s",x2[i].name);
 
-   printf("enter the contact_no of passenger\n");
+   printf("Enter the contact_no of passenger\n");
   scanf("%d",&x2[i].contact_no);
   printf("\n");
-  fail3=payment();
+  fail3=payment(s3);
   //payment process
   // price();
     if(fail3==1){
-    printf("\t\t\t\t\t\t\t***TICKET HAS NOT BOOKED***\n");
+    printf("\t\t\t\t\t\t\t***TICKET WAS NOT BOOKED***\n");
     goto x3;//goto loop
  }
  else{
- printf("\t\t\t\t\tyour ticket is booked sucecessfully for janakpur\n");
+ printf("\t\t\t\t\tYour ticket is booked sucecessfully for Janakpur\n");
  setno2=setno2+1;
 
  printf("\n");
  printf("\t\t\t\t\t\t\tPASSENGER NAME:%s\n",x2[i].name);
  printf("\t\t\t\t\t\t\tcontact_no:%d\n",x2[i].contact_no);
  printf("\t\t\t\t\t\t\tSEATS NO:%d\n",setno2);
-  fprintf(p2,"%s","name:");
+  fprintf(p2,"%s","Name:");
   fprintf(p2,"%s",x2[i].name);
-   fprintf(p2,"%s","contact_no:");
+   fprintf(p2,"%s","Contact_no:");
   fprintf(p2,"%d",x2[i].contact_no);
  
-  fprintf(p2,"%s","seatno:");
+  fprintf(p2,"%s","Seat_no:");
 
   fprintf(p2,"%d",setno2);
   // printf("%d",i);   testing i ;
@@ -315,31 +312,136 @@ scanf("%s",x2[i].name);
 //switch-3 inside destination payment() start
 
 //payment section ESEWA start
-int payment(){
- printf("***PAYMENT VIA ESEWA***\n");
+int payment(int a){
+ printf("***PAYMENT VIA E-SEWA***\n");
     int choice2;
   
     do {
-        printf("\n--- select your payment section---\n");
-        printf("1. pokhara to kathmandu(1000)\n");
-        printf("2. pokhara to chitwan(500)\n");
-        printf("3. pokhara to janakpur(1000)\n");
+        printf("\n--- Select your payment section---\n");
+        printf("1. Pokhara to Kathmandu(1000)\n");
+        printf("2. Pokhara to Chitwan(500)\n");
+        printf("3. Pokhara to Janakpur(1000)\n");
         printf("4. Exit\n");
-           printf("enter your payment section: \n");
+           printf("Enter your payment section: \n");
         scanf("%d", &choice2);
 
         switch (choice2) {
             case 1:
-           
-          return pay1();
+            {
+    int price;
+    int fail=0;
+   
+if(a==2 || a==3){
+    printf("invalid");
+}
+else{
+     printf("Enter the ticket price\n");
+scanf("%d",&price);
+if(price<1000){
+    printf("\t\t\t\t\t\t\t-$$$money is not sufficent$$$-\n");
+    fail=fail+1;
+
+
+  
+    
+}
+else if(price==1000){
+    printf("\t\t\t\t\t\t\t\t****Transaction successful****\n");
+
+}
+else{
+    printf("Transaction successful\n");
+    printf("You will get return of:: %d\n",price-1000);
+}
+//for  unsufficent money
+  if (fail==1){
+   return 1;
+  }
+  else{
+return 0;
+  }
+
+}
+}
+        
         
           
                 break;
             case 2:
-          return pay2();
+          {
+    int price1;
+     int fail1=0;
+  
+if(a==1 || a==3){
+    printf("invalid");
+    
+}
+else{
+  printf("Enter the ticket price\n");
+scanf("%d",&price1);
+if(price1<500){
+    printf("\t\t\t\t\t\t\t-$$$money is not sufficent$$$-\n");
+    
+    fail1=fail1+1;
+    
+}
+else if(price1==500){
+    printf("\t\t\t\t\t\t\t\t****Transaction successful****\n");
+
+}
+else{
+    printf("Transaction successful/n");
+    printf("You will get return of:: %d\n",price1-500);
+}
+//for  unsufficent money
+  if (fail1==1){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+   
+}
                 break;
             case 3:
-        return pay3();
+       {
+      int fail2=0;
+    int price2;
+ 
+if(a==1 || a==2){
+    printf("invalid");
+}else{
+       printf("Enter the ticket price\n");
+scanf("%d",&price2);
+if(price2<1000){
+    printf("\t\t\t\t\t\t\t-$$$money is not sufficent$$$-\n");
+     fail2=fail2+1;
+    
+    
+}
+else if(price2==1000){
+    printf("\t\t\t\t\t\t\t\t****Transaction successful****\n");
+
+}
+else{
+    printf("Transaction successful/n");
+    printf("You will get return of ::%d\n",price2-1000);
+}
+
+//for  unsufficent money
+  if (fail2==1){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+
+}
+   
+   
+}
+
                 break;
             case 4:
                                 printf("Exiting...\n");
@@ -351,104 +453,14 @@ int payment(){
 
 }//switch-3 inside destination payment() end
 
-//switch-3 payment case-1 start
-int pay1(){
-    int price;
-    int fail=0;
-    printf("enter the ticket price\n");
-scanf("%d",&price);
-if(price<1000){
-    printf("\t\t\t\t\t\t\t-$$$money is not sufficent$$$-\n");
-    fail=fail+1;
 
-
-  
-    
-}
-else if(price==1000){
-    printf("\t\t\t\t\t\t\t\t****transition success****\n");
-
-}
-else{
-    printf("transition suceess\n");
-    printf("you will get return of:: %d\n",price-1000);
-}
-//for  unsufficent money
-  if (fail==1){
-    return 1;
-  }
-  else{
-    return 0;
-  }
-}//switch-3 payment case-1 start
-//switch-3 payment case-2 start
-int pay2(){
-    int price1;
-     int fail1=0;
-    printf("enter the ticket price\n");
-scanf("%d",&price1);
-if(price1<500){
-    printf("\t\t\t\t\t\t\t-$$$money is not sufficent$$$-\n");
-    
-    fail1=fail1+1;
-    
-}
-else if(price1==500){
-    printf("\t\t\t\t\t\t\t\t****transition success****\n");
-
-}
-else{
-    printf("transition suceess/n");
-    printf("you will get return of:: %d\n",price1-500);
-}
-//for  unsufficent money
-  if (fail1==1){
-    return 1;
-  }
-  else{
-    return 0;
-  }
-   
-}//switch-3 payment case-2 end
-//switch-3 payment case-3 start
-int pay3(){
-      int fail2=0;
-    int price2;
-    printf("enter the ticket price\n");
-scanf("%d",&price2);
-if(price2<1000){
-    printf("\t\t\t\t\t\t\t-$$$money is not sufficent$$$-\n");
-     fail2=fail2+1;
-    
-    
-}
-else if(price2==1000){
-    printf("\t\t\t\t\t\t\t\t****transition sucess****\n");
-
-}
-else{
-    printf("transition suceess/n");
-    printf("you will get return of ::%d\n",price2-1000);
-}
-//for  unsufficent money
-  if (fail2==1){
-    return 1;
-  }
-  else{
-    return 0;
-  }
-   
-   
-}//switch-3 payment case-3 end
-
-//switch-4 data access case-1 start
-
+//file handling
 int view_passenger(){
     FILE *p;
     char c;
    p=fopen("bus.txt","r");
    if(p==NULL){
-       printf("error");
+       printf("Error");
        
    }
    while((c=fgetc(p))!=EOF){
@@ -464,7 +476,7 @@ int view_passenger2(){
     char c;
    p1=fopen("bus1.txt","r");
    if(p1==NULL){
-       printf("error");
+       printf("Error");
        
    }
    while((c=fgetc(p1))!=EOF){
@@ -472,15 +484,13 @@ int view_passenger2(){
    }
    fclose(p1);
    return 0;
-}//switch-4 data access case-2 end
-
-//switch-4 data access case-3 start
+}
 int view_passenger3(){
     FILE *p2;
     char c;
    p2=fopen("bus2.txt","r");
    if(p2==NULL){
-       printf("error");
+       printf("Error");
        
    }
    while((c=fgetc(p2))!=EOF){
@@ -490,7 +500,6 @@ int view_passenger3(){
    return 0;
 }
 
-//switch-4 data access case-3 end
 
 
 
@@ -499,7 +508,7 @@ int view_passenger3(){
 //ESEWA end
 //process
 /*date 2081-01-07
--use of goto loop for money transition,use of return value from one function to another
+-use of goto loop for money Transaction,use of return value from one function to another
 -line formating
 -addition of file feature for more 2 buses
 */
@@ -511,7 +520,7 @@ int view_passenger3(){
 3.use of array//
 4.use of value and argument pass
 5.documentation//
-6.formatted funcstion//
+6.formatted function//
 
 */
 //date:2081-01-08
